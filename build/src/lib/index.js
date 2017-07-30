5a1ce8f17b3cb4124e07befa511689b86cef97a6
Enache Catalin <enachecatalin@Enaches-MacBook-Pro.local>
2017-07-30 14:49:02 +0300

    browserify experiments

--- build/src/lib/index.js
+++ build/src/lib/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.List = exports.Hello = undefined;
+exports.List = exports.Hello = exports.ThemeProvider = undefined;
 
 var _Hello = require('./components/Hello/Hello');
 
@@ -13,7 +13,10 @@ var _List = require('./components/List/List');
 
 var _List2 = _interopRequireDefault(_List);
 
+var _reactJss = require('react-jss');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+exports.ThemeProvider = _reactJss.ThemeProvider;
 exports.Hello = _Hello2.default;
 exports.List = _List2.default;
--- src/lib/index.js
+++ src/lib/index.js
@@ -1,7 +1,9 @@
 import Hello from './components/Hello/Hello';
 import List from './components/List/List';
+import { ThemeProvider } from 'react-jss';
 
 export {
+    ThemeProvider,
     Hello,
     List
 }
