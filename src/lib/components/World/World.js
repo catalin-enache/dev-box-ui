f89c737d42f2efe1acba39e5bf0c3e3814d83cca
Enache Catalin <enachecatalin@Enaches-MacBook-Pro.local>
2017-08-09 23:04:00 +0300

    theme experiments

--- src/lib/components/World/World.js
+++ src/lib/components/World/World.js
@@ -2,40 +2,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import List from '../List/List';
-import World from '../World/World';
 import themeAware from '../../HOC/themeAware';
 import FaSpinner from 'react-icons/lib/fa/spinner';
 
 const style = (theme) => {
     return {
-      hello: {
+      world: {
         color: theme.primaryTextColor || 'orange'
       }
     };
 };
 
-class Hello extends React.Component {
+class World extends React.Component {
     render() {
         if (process.env.NODE_ENV !== 'production') {
             console.log('rendering Hello component');
         }
         return (
                 <div className={this.props.classes.hello}>
-                    Hello {this.props.name || 'Nobody'}
-                    <FaSpinner className={this.props.classes.faSpin} />
-                    <List items={['one', 'two']} />
-                    <List items={['one', 'two']} />
-                    <World/>
-                    <World/>
+                    World ------------
+                    <List items={['five', 'six']} />
+                    <List items={['five', 'six']} />
+                    ------------------
                 </div>
         );
     }
 }
 
-Hello.propTypes = {
-    name: PropTypes.string.isRequired,
+World.propTypes = {
     classes: PropTypes.object
 };
 
-export default themeAware({ style })(Hello);
+export default themeAware({ style })(World);
 
