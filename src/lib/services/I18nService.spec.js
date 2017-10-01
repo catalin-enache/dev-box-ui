95c10d39a85f21628657efda73889d637704ac85
Enache Catalin <enachecatalin@Catalins-MacBook-Pro.local>
2017-10-01 16:03:55 +0300

    further changes

--- src/lib/services/I18nService.spec.js
+++ src/lib/services/I18nService.spec.js
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
-import I18nService from './I18nService';
-import LocaleService from './LocaleService';
+import i18nService from './I18nService';
+import localeService from './LocaleService';
 
 const lang1 = 'xy';
 const lang2 = 'yz';
@@ -21,43 +21,43 @@ const lang2Translations1 = {
   },
 };
 
-describe('I18nService', () => {
+describe('i18nService', () => {
   it('registers new translations', () => {
-    I18nService.clearTranslations(lang1);
-    expect(I18nService.translations[lang1]).to.equal(undefined);
-    I18nService.registerTranslations(lang1Translations1);
-    expect(I18nService.translations[lang1]).to.deep.equal({
+    i18nService.clearTranslations(lang1);
+    expect(i18nService.translations[lang1]).to.equal(undefined);
+    i18nService.registerTranslations(lang1Translations1);
+    expect(i18nService.translations[lang1]).to.deep.equal({
       ...lang1Translations1[lang1]
     });
-    I18nService.registerTranslations(lang1Translations2);
-    expect(I18nService.translations[lang1]).to.deep.equal({
+    i18nService.registerTranslations(lang1Translations2);
+    expect(i18nService.translations[lang1]).to.deep.equal({
       ...lang1Translations1[lang1],
       ...lang1Translations2[lang1]
     });
-    I18nService.clearTranslations(lang1);
-    expect(I18nService.translations[lang1]).to.equal(undefined);
+    i18nService.clearTranslations(lang1);
+    expect(i18nService.translations[lang1]).to.equal(undefined);
   });
 
   it('is locale aware, it reacts to locale changes', (done) => {
-    I18nService.clearTranslations(lang1);
-    I18nService.clearTranslations(lang2);
+    i18nService.clearTranslations(lang1);
+    i18nService.clearTranslations(lang2);
 
-    I18nService.registerTranslations({
+    i18nService.registerTranslations({
       ...lang1Translations1,
       ...lang2Translations1
     });
 
-    LocaleService.locale = { lang: lang1 };
+    localeService.locale = { lang: lang1 };
     setTimeout(() => {
-      expect(I18nService.currentLangTranslations).to.deep.equal(lang1Translations1[lang1]);
-      expect(I18nService.translate('one')).to.equal(lang1Translations1[lang1].one);
+      expect(i18nService.currentLangTranslations).to.deep.equal(lang1Translations1[lang1]);
+      expect(i18nService.translate('one')).to.equal(lang1Translations1[lang1].one);
 
-      LocaleService.locale = { lang: lang2 };
+      localeService.locale = { lang: lang2 };
       setTimeout(() => {
-        expect(I18nService.currentLangTranslations).to.deep.equal(lang2Translations1[lang2]);
-        expect(I18nService.translate('one')).to.equal(lang2Translations1[lang2].one);
+        expect(i18nService.currentLangTranslations).to.deep.equal(lang2Translations1[lang2]);
+        expect(i18nService.translate('one')).to.equal(lang2Translations1[lang2].one);
 
-        LocaleService.locale = { lang: 'en' };
+        localeService.locale = { lang: 'en' };
         setTimeout(() => {
           done();
         }, 0);
