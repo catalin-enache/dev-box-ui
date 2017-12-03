caf100c1764c1bf6ff435a3d6165803b39af6946
Enache Catalin <enachecatalin@Catalins-MacBook-Pro.local>
2017-12-03 15:09:02 +0200

    further changes

--- src/lib/webcomponents/index.js
+++ src/lib/webcomponents/index.js
@@ -2,11 +2,7 @@
 import getDBUWebComponentDummy from './DBUWebComponentDummy/DBUWebComponentDummy';
 import getDBUWebComponentDummyParent from './DBUWebComponentDummyParent/DBUWebComponentDummyParent';
 
-const DBUWebComponents = {
-  DBUWebComponentDummy: getDBUWebComponentDummy(window),
-  DBUWebComponentDummyParent: getDBUWebComponentDummyParent(window)
+export {
+  getDBUWebComponentDummy,
+  getDBUWebComponentDummyParent
 };
-
-window.DBUWebComponents = DBUWebComponents;
-
-Object.keys(DBUWebComponents).forEach((component) => component.registerSelf());
