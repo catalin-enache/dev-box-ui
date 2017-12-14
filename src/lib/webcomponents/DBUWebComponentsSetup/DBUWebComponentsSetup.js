bba15b1682b189983721bb7a6cef219b506454c3
Enache Catalin <enachecatalin@Catalins-MacBook-Pro.local>
2017-12-14 12:39:48 +0200

    further changes

--- src/lib/webcomponents/DBUWebComponentsSetup/DBUWebComponentsSetup.js
+++ src/lib/webcomponents/DBUWebComponentsSetup/DBUWebComponentsSetup.js
@@ -1,22 +1,4 @@
-
-/*
-DBUWebComponentBase (from which all web-components inherit)
-will read componentStyle from win.DBUWebComponents
-when klass.registerSelf() is called giving a chance to override default web-component style
-just before it is registered.
-*/
-const appendStyle = (win) => (registrationName, componentStyle) => {
-  if (!win.DBUWebComponents) {
-    win.DBUWebComponents = {};
-  }
-  win.DBUWebComponents = {
-    ...win.DBUWebComponents,
-    [registrationName]: {
-      ...win.DBUWebComponents[registrationName],
-      componentStyle
-    }
-  };
-};
+import appendStyle from '../internals/appendStyle';
 
 export default function dbuWebComponentsSetUp(win) {
   return {
---
+++ src/lib/webcomponents/internals/appendStyle.js
@@ -0,0 +1,20 @@
+/*
+DBUWebComponentBase (from which all web-components inherit)
+will read componentStyle from win.DBUWebComponents
+when klass.registerSelf() is called giving a chance to override default web-component style
+just before it is registered.
+*/
+const appendStyle = (win) => (registrationName, componentStyle) => {
+  if (!win.DBUWebComponents) {
+    win.DBUWebComponents = {};
+  }
+  win.DBUWebComponents = {
+    ...win.DBUWebComponents,
+    [registrationName]: {
+      ...win.DBUWebComponents[registrationName],
+      componentStyle
+    }
+  };
+};
+
+export default appendStyle;
