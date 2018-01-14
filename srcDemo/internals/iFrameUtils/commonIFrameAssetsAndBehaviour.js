b3ae1037b8b342605a5b67cd9e86c015720e9ed6
Enache Catalin <enachecatalin@Catalins-MacBook-Pro.local>
2018-01-14 12:14:18 +0200

    further changes

--- srcDemo/internals/iFrameUtils/commonIFrameAssetsAndBehaviour.js
+++ srcDemo/internals/iFrameUtils/commonIFrameAssetsAndBehaviour.js
@@ -13,7 +13,8 @@ window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = window.parent.__REACT_DEVTOOLS_GLOBAL_HO
       '';
   if (!isEmbeded) {
     const screen = href.split('?')[0].substr(href.indexOf(rootUrl) + rootUrl.length);
-    window.location.replace(`${window.location.origin}${originSubPath}?screen=${screen}`);
+    console.log({screen})
+    // window.location.replace(`${window.location.origin}${originSubPath}?screen=${screen}`);
   }
 })();
 
