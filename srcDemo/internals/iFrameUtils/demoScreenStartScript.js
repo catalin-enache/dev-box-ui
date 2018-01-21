17971fa8a067671d1eddbaa9cc945a79319d0c76
Enache Catalin <enachecatalin@Catalins-MacBook-Pro.local>
2018-01-21 13:33:57 +0200

    further changes

--- srcDemo/internals/iFrameUtils/demoScreenStartScript.js
+++ srcDemo/internals/iFrameUtils/demoScreenStartScript.js
@@ -1,6 +1,6 @@
 
 window.distributionURL =
-  'https://catalin-enache.github.io/dev-box-ui/build/dist/js/dev-box-ui-web-components.js';
+  'https://unpkg.com/dev-box-ui/dist/js/dev-box-ui-web-components.min.js';
 
 console.log('hiding body');
 document.querySelector('.demo-screen').style.display = 'none';
