148bc34f12c88bcd87374542cf91f1830768ef53
Enache Catalin <enachecatalin@Catalins-MacBook-Pro.local>
2018-01-24 16:35:38 +0200

    further changes

--- src/lib/web-components/services/DBUILocaleService.js
+++ src/lib/web-components/services/DBUILocaleService.js
@@ -14,7 +14,7 @@ export default function getDBUILocaleService(win) {
       constructor() {
         this._callbacks = [];
         this._localeAttrs = Object.keys(defaultLocale);
-        this._rootElement = window.document.documentElement;
+        this._rootElement = win.document.documentElement;
         this._localeAttrs.forEach((attr) => {
           if (!this._rootElement.getAttribute(attr)) {
             this._rootElement.setAttribute(attr, defaultLocale[attr]);
