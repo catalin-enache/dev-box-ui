83d1bc87a82a13a4c9c62446b1cb062566978dbd
Enache Catalin <enachecatalin@Catalins-MacBook-Pro.local>
2018-05-15 09:58:48 +0300

    added new styling behaviour unit test

--- src/lib/core/web-components/components/DBUIWebComponentCore/DBUIWebComponentStylingBehaviour.spec.js
+++ src/lib/core/web-components/components/DBUIWebComponentCore/DBUIWebComponentStylingBehaviour.spec.js
@@ -1,5 +1,4 @@
 import { expect } from 'chai';
-import sinon from 'sinon';
 import getDBUIWebComponentCore from './DBUIWebComponentCore';
 import DBUICommonCssVars from './DBUICommonCssVars';
 import ensureSingleRegistration from '../../../internals/ensureSingleRegistration';
@@ -8,7 +7,7 @@ import inIframe from '../../../../../../testUtils/inIframe';
 
 
 const dummyOneRegistrationName = 'dummy-one';
-const dummyOneStyle = '';
+const dummyOneStyle = ':host { display: block; }';
 function getDummyOne(win) {
   return ensureSingleRegistration(win, dummyOneRegistrationName, () => {
     const {
@@ -17,18 +16,29 @@ function getDummyOne(win) {
       Registerable
     } = getDBUIWebComponentCore(win);
 
+    const DummyTwo = getDummyTwo(win);
+    const DummyThree = getDummyThree(win);
+
     class DummyOne extends DBUIWebComponentBase {
 
       static get registrationName() {
         return dummyOneRegistrationName;
       }
 
+      static get dependencies() {
+        return [DummyTwo, DummyThree];
+      }
+
       static get templateInnerHTML() {
         return `
           <style>${dummyOneStyle}</style>
           <div>
-            <p>dummy one component</p>
-            <slot></slot>
+            <div>[dummy one]</div>
+            <dummy-two>
+              <dummy-three>
+                <slot></slot>
+              </dummy-three>
+            </dummy-two>
           </div>
         `;
       }
@@ -43,34 +53,28 @@ function getDummyOne(win) {
   });
 }
 
-const dummyOneParentRegistrationName = 'dummy-one-parent';
-const dummyOneParentStyle = '';
-function getDummyOneParent(win) {
-  return ensureSingleRegistration(win, dummyOneParentRegistrationName, () => {
+const dummyTwoRegistrationName = 'dummy-two';
+const dummyTwoStyle = ':host { display: block; }';
+function getDummyTwo(win) {
+  return ensureSingleRegistration(win, dummyTwoRegistrationName, () => {
     const {
       DBUIWebComponentBase,
       defineCommonStaticMethods,
       Registerable
     } = getDBUIWebComponentCore(win);
 
-    const DummyOne = getDummyOne(win);
-
-    class DummyOneParent extends DBUIWebComponentBase {
+    class DummyTwo extends DBUIWebComponentBase {
 
       static get registrationName() {
-        return dummyOneParentRegistrationName;
-      }
-
-      static get dependencies() {
-        return [DummyOne];
+        return dummyTwoRegistrationName;
       }
 
       static get templateInnerHTML() {
         return `
-          <style>${dummyOneParentStyle}</style>
+          <style>${dummyTwoStyle}</style>
           <div>
-            <p>dummy one parent component</p>
-            <dummy-one><slot></slot></dummy-one>
+            <div>[dummy two]</div>
+            <slot></slot>
           </div>
         `;
       }
@@ -79,7 +83,86 @@ function getDummyOneParent(win) {
 
     return Registerable(
       defineCommonStaticMethods(
-        DummyOneParent
+        DummyTwo
+      )
+    );
+  });
+}
+
+const dummyThreeRegistrationName = 'dummy-three';
+const dummyThreeStyle = ':host { display: block; }';
+function getDummyThree(win) {
+  return ensureSingleRegistration(win, dummyThreeRegistrationName, () => {
+    const {
+      DBUIWebComponentBase,
+      defineCommonStaticMethods,
+      Registerable
+    } = getDBUIWebComponentCore(win);
+
+    const DummyFour = getDummyFour(win);
+
+    class DummyThree extends DBUIWebComponentBase {
+
+      static get registrationName() {
+        return dummyThreeRegistrationName;
+      }
+
+      static get dependencies() {
+        return [DummyFour];
+      }
+
+      static get templateInnerHTML() {
+        return `
+          <style>${dummyThreeStyle}</style>
+          <div>
+            <div>[dummy three]</div>
+            <dummy-four></dummy-four>
+            <slot></slot>
+          </div>
+        `;
+      }
+
+    }
+
+    return Registerable(
+      defineCommonStaticMethods(
+        DummyThree
+      )
+    );
+  });
+}
+
+const dummyFourRegistrationName = 'dummy-four';
+const dummyFourStyle = ':host { display: block; }';
+function getDummyFour(win) {
+  return ensureSingleRegistration(win, dummyFourRegistrationName, () => {
+    const {
+      DBUIWebComponentBase,
+      defineCommonStaticMethods,
+      Registerable
+    } = getDBUIWebComponentCore(win);
+
+    class DummyFour extends DBUIWebComponentBase {
+
+      static get registrationName() {
+        return dummyFourRegistrationName;
+      }
+
+      static get templateInnerHTML() {
+        return `
+          <style>${dummyFourStyle}</style>
+          <div>
+            <div>[dummy four]</div>
+            <slot></slot>
+          </div>
+        `;
+      }
+
+    }
+
+    return Registerable(
+      defineCommonStaticMethods(
+        DummyFour
       )
     );
   });
@@ -243,6 +326,87 @@ describe('DBUIWebComponent Styling Behaviour', () => {
             // falling back to default dir "ltr" when target dir is falsy
             expect(computedStyle1.color).to.equal('rgba(0, 0, 255, 0.5)');
             expect(computedStyle2.color).to.equal('rgba(0, 0, 255, 0.5)');
+
+            iframe.remove();
+            done();
+          }, 0);
+        });
+
+        DummyOne.registerSelf();
+      }
+    });
+  });
+
+  it('allows injecting custom css vars in shadow dom and control styling from outside', (done) => {
+    inIframe({
+      headStyle: `
+      dummy-one {
+        color: rgb(240, 240, 0);
+        --dummy-two-color: rgb(255, 0, 0);
+        --dummy-three-color: rgb(0, 255, 0);
+        --dummy-four-color: rgb(0, 0, 255);
+      }
+      dummy-one.two {
+        color: rgb(240, 240, 0);
+        --dummy-two-color: rgb(255, 255, 0);
+        --dummy-three-color: rgb(255, 0, 255);
+        --dummy-four-color: rgb(0, 255, 255);
+      }
+      .one span, .two span { color: rgb(0, 0, 0); }
+      `,
+      bodyHTML: `
+        <dummy-one class="one"><span>foo</span></dummy-one>
+        <dummy-one class="two"><span>bar</span></dummy-one>
+      `,
+      onLoad: ({ contentWindow, iframe }) => {
+        const DummyOne = getDummyOne(contentWindow);
+        // const DummyTwo = getDummyTwo(contentWindow);
+        // const DummyThree = getDummyThree(contentWindow);
+        const DummyFour = getDummyFour(contentWindow);
+        DummyOne.componentStyle += `
+          dummy-two {
+            color: var(--dummy-two-color, inherit);
+          }
+          dummy-three {
+            color: var(--dummy-three-color, inherit);
+          }
+        `;
+        DummyFour.componentStyle += `
+          :host {
+            color: var(--dummy-four-color, inherit);
+          }
+        `;
+
+        contentWindow.customElements.whenDefined(DummyOne.registrationName).then(() => {
+
+          const dummyOneInst1 = contentWindow.document.querySelector('.one');
+          const dummyOneInst1DummyTwo = dummyOneInst1.shadowRoot.querySelector('dummy-two');
+          const dummyOneInst1DummyThree = dummyOneInst1.shadowRoot.querySelector('dummy-three');
+          const dummyOneInst1DummyFour = dummyOneInst1DummyThree.shadowRoot.querySelector('dummy-four');
+
+          const dummyOneInst2 = contentWindow.document.querySelector('.two');
+          const dummyOneInst2DummyTwo = dummyOneInst2.shadowRoot.querySelector('dummy-two');
+          const dummyOneInst2DummyThree = dummyOneInst2.shadowRoot.querySelector('dummy-three');
+          const dummyOneInst2DummyFour = dummyOneInst2DummyThree.shadowRoot.querySelector('dummy-four');
+
+          expect(contentWindow.getComputedStyle(dummyOneInst1).color).to.equal('rgb(240, 240, 0)');
+          expect(contentWindow.getComputedStyle(dummyOneInst1DummyTwo).color).to.equal('rgb(255, 0, 0)');
+          expect(contentWindow.getComputedStyle(dummyOneInst1DummyThree).color).to.equal('rgb(0, 255, 0)');
+          expect(contentWindow.getComputedStyle(dummyOneInst1DummyFour).color).to.equal('rgb(0, 0, 255)');
+          expect(contentWindow.getComputedStyle(dummyOneInst2).color).to.equal('rgb(240, 240, 0)');
+          expect(contentWindow.getComputedStyle(dummyOneInst2DummyTwo).color).to.equal('rgb(255, 255, 0)');
+          expect(contentWindow.getComputedStyle(dummyOneInst2DummyThree).color).to.equal('rgb(255, 0, 255)');
+          expect(contentWindow.getComputedStyle(dummyOneInst2DummyFour).color).to.equal('rgb(0, 255, 255)');
+
+          dummyOneInst1.style.setProperty('--dummy-four-color', 'rgb(0, 255, 0)');
+
+          expect(contentWindow.getComputedStyle(dummyOneInst1DummyFour).color).to.equal('rgb(0, 255, 0)');
+
+          dummyOneInst1.style.removeProperty('--dummy-four-color');
+
+          expect(contentWindow.getComputedStyle(dummyOneInst1DummyFour).color).to.equal('rgb(0, 0, 255)');
+
+          setTimeout(() => {
 
             iframe.remove();
             done();
